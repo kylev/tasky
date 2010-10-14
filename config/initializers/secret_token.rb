5e58bac871a43450c3ad7d9b968251f41c3a9b7f
Kyle VanderBeek <kylev@kylev.com>
2010-10-13 22:44:27 -0700

    Start my homework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasky::Application.config.secret_token = '4b7bbf94ba485ddff4e37d4e58cb88c58bdab758ff5f2fdcb23bebe65475c5b7d70f6c95994b7866d78368427aef255f3b2cc3e6b29b716ac14ea5bc41c7786b'
